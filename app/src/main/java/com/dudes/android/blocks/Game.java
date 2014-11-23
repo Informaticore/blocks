3f0d3ade21d9b5e83a101a4ad9a86fabfd1fb788
Mirko Hberlin <mirko.haeberlin@gmail.com>
2014-11-23 14:13:27 +0100

    added flying clouds + blocks + animation destroy

--- app/src/main/java/com/dudes/android/blocks/Game.java
+++ app/src/main/java/com/dudes/android/blocks/Game.java
@@ -1,9 +1,7 @@
 package com.dudes.android.blocks;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
+import android.graphics.*;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -14,6 +12,7 @@ import android.view.View;
  */
 public class Game extends View {
 
+    private final Bitmap mHud;
     private float mThingX;
     private float mThingY;
     private Paint mThingPaint;
@@ -24,12 +23,15 @@ public class Game extends View {
         mThingPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mThingPaint.setColor(Color.RED);
         mThingPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+        mHud = BitmapFactory.decodeResource(getResources(), R.drawable.hud);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawCircle(mThingX, mThingY, 30, mThingPaint);
+//        canvas.drawCircle(mThingX, mThingY, 30, mThingPaint);
+        canvas.drawBitmap(mHud, mThingX , mThingY, mThingPaint);
     }
 
     @Override
@@ -37,10 +39,10 @@ public class Game extends View {
         int action = event.getAction();
         switch(action){
             case MotionEvent.ACTION_DOWN:
-                mThingX = event.getX();
-                mThingY = event.getY();
+                mThingX = event.getX() - mHud.getWidth() /2;
+                mThingY = event.getY() - mHud.getHeight() /2;
                 invalidate();
-                break;
+                return false;
         }
         return super.onTouchEvent(event);
     }
